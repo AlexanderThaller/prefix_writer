47dcdd492af60edd5371ac9f8f3bb391b9e74430
Alexander Thaller <alexander.thaller@trivago.com>
2021-11-25 12:16:16 +0100

    Enable building on stable

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 #![warn(clippy::pedantic)]
 #![warn(clippy::unwrap_used)]
 #![warn(rust_2018_idioms, unused_lifetimes, missing_debug_implementations)]
-#![feature(test)]
 
 //! Crate for a writer that can prefix text that contains multiple
 //! lines or incomplete lines.
@@ -97,51 +96,6 @@ mod test {
     use super::PrefixWriter;
 
     const PREFIX: &str = "prefix: ";
-
-    const LOREM_IPSUM: &str =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nisi vitae nisl \
-         ullamcorper, eget ullamcorper dolor dignissim. Etiam tempus elit vitae sem euismod \
-         tempor. Ut vestibulum lacus lorem. Class aptent taciti sociosqu ad litora torquent per \
-         conubia nostra, per inceptos himenaeos. Nulla rutrum congue nibh, vitae dapibus nunc \
-         efficitur sit amet. Phasellus et cursus enim. Aenean ultrices augue nec velit eleifend \
-         ultricies. Mauris viverra dictum enim, ut lobortis enim finibus ut. Vestibulum pharetra \
-         velit lacus, vitae placerat nulla gravida eget.
-
-Cras neque magna, tempor condimentum nunc vitae, ultrices scelerisque nisl. Proin vitae tincidunt \
-         massa, et placerat nunc. Vivamus imperdiet mauris id lectus porttitor commodo. Etiam \
-         facilisis congue luctus. Integer ut elit facilisis orci ullamcorper porta id sed est. \
-         Sed maximus elit ut lorem auctor porttitor. Curabitur eget mi vitae libero suscipit \
-         euismod. Mauris placerat nunc ac lorem placerat, sit amet euismod mi aliquam. Donec \
-         tristique tortor et ex dignissim congue. Curabitur luctus ante et nisl ullamcorper \
-         maximus et at ex. Suspendisse in consequat nunc. Morbi dapibus, ipsum quis dictum \
-         tincidunt, mauris augue dignissim eros, non commodo ante neque feugiat purus. Proin \
-         faucibus sodales nisi non posuere. Maecenas facilisis egestas dui, nec faucibus nibh \
-         mattis vel. Cras facilisis nibh sit amet bibendum vestibulum.
-
-Nulla vulputate sem ante, in ultrices quam placerat et. Morbi nec urna suscipit, hendrerit ante \
-         quis, lobortis purus. Nam tempor, odio non euismod venenatis, lorem ex elementum neque, \
-         at rutrum erat enim et sem. Praesent in risus dapibus, finibus tellus nec, bibendum \
-         arcu. Donec eget tellus a neque tincidunt dictum nec ac neque. Vestibulum id nibh et ex \
-         porttitor mattis. Aenean rhoncus nisl nibh. Nam quis rhoncus lorem, id ultrices dui. \
-         Curabitur sodales quam condimentum nisl dapibus, dapibus finibus leo varius. Etiam vel \
-         mi in urna vestibulum consequat. Mauris hendrerit tortor ex, quis tempus nisi auctor \
-         nec. Cras accumsan est ut massa aliquam viverra. Integer egestas nibh vel nulla egestas, \
-         in vulputate sapien finibus.
-
-Mauris egestas, lectus vitae vehicula suscipit, tellus quam hendrerit magna, nec fringilla ex \
-         mauris non justo. Donec purus purus, interdum facilisis erat non, iaculis vulputate \
-         velit. Nulla semper vehicula tortor, eu dignissim lorem posuere ac. Cras sollicitudin, \
-         dui eu pretium ullamcorper, massa lectus suscipit diam, ac volutpat nunc mi ut massa. \
-         Nullam tincidunt sit amet libero in tincidunt. In sagittis in libero id bibendum. \
-         Phasellus bibendum lacus vel ultricies tincidunt.
-
-Proin diam enim, maximus quis risus et, imperdiet auctor dui. Quisque nisi metus, rhoncus \
-         imperdiet laoreet at, lacinia aliquam arcu. Aenean lobortis pellentesque justo, nec \
-         consectetur magna vulputate vel. Cras facilisis vestibulum sem, eu maximus sem dictum \
-         eget. Aenean imperdiet enim sed tempus eleifend. Proin in velit quis ante eleifend \
-         egestas. Fusce iaculis magna vitae sodales interdum. Aenean id nunc velit. Pellentesque \
-         bibendum metus porta augue vehicula tempus. Integer blandit tellus velit, quis pretium \
-         eros venenatis non.";
 
     fn give_random_input() -> Vec<u8> {
         let mut rng = rand::thread_rng();
@@ -254,69 +208,6 @@ Proin diam enim, maximus quis risus et, imperdiet auctor dui. Quisque nisi metus
                     assert!(line.starts_with(PREFIX))
                 }
             }
-        }
-    }
-
-    mod bench {
-        #![allow(soft_unstable)]
-
-        extern crate test;
-
-        use test::{
-            black_box,
-            Bencher,
-        };
-
-        use super::{
-            PrefixWriter,
-            Write,
-            LOREM_IPSUM,
-            PREFIX,
-        };
-
-        fn run(b: &mut Bencher, input: &str, prefix: &str) {
-            let mut buffer = Vec::new();
-            let mut writer = PrefixWriter::new(prefix.to_owned(), &mut buffer);
-            b.iter(|| {
-                black_box(writer.write_all(input.as_bytes()).unwrap());
-            });
-
-            writer.flush().unwrap();
-
-            let _got = String::from_utf8_lossy(&buffer);
-        }
-
-        #[bench]
-        fn empty(b: &mut Bencher) {
-            const INPUT: &str = "";
-
-            run(b, INPUT, PREFIX);
-        }
-
-        #[bench]
-        fn single_line(b: &mut Bencher) {
-            const INPUT: &str = "first\n";
-
-            run(b, INPUT, PREFIX);
-        }
-
-        #[bench]
-        fn two_line(b: &mut Bencher) {
-            const INPUT: &str = "first\nsecond\n";
-
-            run(b, INPUT, PREFIX);
-        }
-
-        #[bench]
-        fn two_line_remainder(b: &mut Bencher) {
-            const INPUT: &str = "first\nsecond";
-
-            run(b, INPUT, PREFIX);
-        }
-
-        #[bench]
-        fn lorem_ipsum(b: &mut Bencher) {
-            run(b, LOREM_IPSUM, PREFIX);
         }
     }
 }
